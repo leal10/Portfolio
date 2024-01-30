9254366589090770aa66a3a2497744bbc3d1b8b0
Daniel Leal <leadaniel456@gmail.com>
2024-01-29 19:37:57 -0600

    Updated the folders for the projects

---
+++ my-portfolio/app/navbar.js
@@ -0,0 +1,5 @@
+import styles from './navbar.module.css'
+
+export default function Navbar() {
+  return <div className={styles.navbar}>We will place the navbar here</div>
+}
--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Navbar from './navbar.js'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Navbar />
       <div className={styles.description}>Hello World!</div>
     </main>
   )
