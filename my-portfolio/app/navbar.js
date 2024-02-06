8d1d420f1b52a30e809ae786d32c8a1707fa02dc
Daniel Leal <leadaniel456@gmail.com>
2024-02-05 22:03:55 -0600

    Navbar layout

--- my-portfolio/app/navbar.js
+++ my-portfolio/app/navbar.js
@@ -1,5 +1,15 @@
 import styles from './navbar.module.css'
 
 export default function Navbar() {
-  return <div className={styles.navbar}>We will place the navbar here</div>
+  return (
+    <div className={styles.navbarContainer}>
+      <div className={styles.navbarLogo}>DL</div>
+      <div className={styles.navbarLinks}>
+        <p>Home</p>
+        <p>Tech</p>
+        <p>Projects</p>
+        <p>Contact</p>
+      </div>
+    </div>
+  )
 }
